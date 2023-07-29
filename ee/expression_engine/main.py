199b9b2d7f8bf556b80b883a3cb71afeade58174
anyangml <anyangpeng.ca@gmail.com>
2023-07-29 09:36:51 -0400

    feat: add prompt and gpt to ee

--- ee/expression_engine/main.py
+++ ee/expression_engine/main.py
@@ -1,21 +1,23 @@
 import gradio as gr
-from util import ORChelper
+from util import OCRhelper
+
 
 class Gradio:
 
-    ocr = ORChelper()
+    ocr = OCRhelper()
 
     def launch(self):
         demo = gr.Interface(
-            fn=Gradio.ocr.combine,
+            fn=Gradio.ocr.pipeline,
             inputs=[
-                gr.inputs.Image(source="upload", type="pil",label = "Latex Image"),
-                gr.inputs.Image(source="upload", type="pil",label = "Document Image"),
+                gr.inputs.Image(source="upload", type="pil", label="Latex Image"),
+                gr.inputs.Image(source="upload", type="pil", label="Document Image"),
             ],
             outputs=["text"],
             cache_examples=False,
         )
         demo.launch()
+
 
 if __name__ == "__main__":
     Gradio().launch()
